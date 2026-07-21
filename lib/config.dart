e6002b76cdf3335d342a9b2cf3d1e1996597311c
David.M.Costa <david.moreirarj27@gmail.com>
2026-07-21 01:26:19 -0300

    Set default frontend backend base to http for broader local compatibility

--- lib/config.dart
+++ lib/config.dart
@@ -1,8 +1,8 @@
 // Copy this file to lib/config.dart and update values for your environment if needed.
-// Default values point to the hosted domain filmes-series.com for convenience.
+// Default values point to the local films-series.com hostname for convenience.
 
 const String TMDB_API_KEY = 'YOUR_TMDB_KEY';
-const String BACKEND_BASE = 'https://filmes-series.com:4000';
+const String BACKEND_BASE = 'http://filmes-series.com:4000';
 
 // For local Android emulator, you may need to use http://10.0.2.2:4000.
 // For desktop/web, use https://filmes-series.com:4000 if mkcert is configured locally.
